2847c1a3eff743a4fd57a11929ed7ca5991ec935
LAB24_USER ON WS27 <L24Y08W27@LabHCI.net>
2026-03-05 08:14:29 +0800

    docs: initiliazed frontend and backend directories

---
+++ backend/flashcardfrenzy/src/main/java/com/marikit/flashcardfrenzy/FlashcardfrenzyApplication.java
@@ -0,0 +1,13 @@
+package com.marikit.flashcardfrenzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardfrenzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardfrenzyApplication.class, args);
+	}
+
+}
---
+++ backend/flashcardfrenzy/src/test/java/com/marikit/flashcardfrenzy/FlashcardfrenzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marikit.flashcardfrenzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardfrenzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
